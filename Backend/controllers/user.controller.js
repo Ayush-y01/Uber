062568476bd0ea7ea95586e1a9e6850e591d2d2d
Ayush-y01 <Ayush-y01>
2025-12-02 20:42:22 +0530

    user basic auth done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model.js');
 const userService = require('../services/user.service.js');
 const { validationResult } = require('express-validator')
+const authMiddleware = require('../middlewares/auth.middleware.js')
+const BlacklistTokenModel = require('../models/blacklistToken.model.js')
 
 
 module.exports.registerUser = async(req, res, next) => {
@@ -50,5 +52,22 @@ module.exports.loginUser = async (req, res, next) => {
 
   const token = user.generateAuthToken();
 
+  res.cookie('token', token);
+
   res.status(200).json({ token, user });
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+  
+  
+  res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie('token');
+  const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+
+  await blacklistTokenModel.create({ token });
+
+  res.status(200).json({message: 'Logged out'})
 }
--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { body } = require('express-validator');
 const router = express.Router();
 const userController = require('../controllers/user.controller.js');
+const authMiddleware = require('../middlewares/auth.middleware.js')
 
 
 router.post('/register',[
@@ -20,6 +21,8 @@ router.post('/login', [
     userController.loginUser
 )
 
+router.get('/profile', authMiddleware.authUser, userController.getUserProfile);
+router.get('/logout', authMiddleware.authUser, userController.logoutUser);
 
 
 
