bbfc80a14afddf2786482f1792d441d012e0c2b1
Ayush-y01 <Ayush-y01>
2025-12-12 23:34:08 +0530

    no major change

--- frontend/Uber/src/pages/UserSignup.jsx
+++ frontend/Uber/src/pages/UserSignup.jsx
@@ -1,92 +1,107 @@
-
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function UserSignup() {
-    return ( <div>
-        <h1>UserSignup</h1>
-    </div> );
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userData, setUserData] = useState("");
 
-export default UserSignup;
-import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-const UserSignup = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+    setUserData({
+        username:{
+            firstName:firstName,
+            lastName:lastName
+        },
+        email:email,
+        password:password
+    })
+    
+    setFirstName('')
+    setLastName('')
+    setEmail('')
+    setPassword('')
 
-
-    const submitHandler = (e) =>{
-        e.preventDefault()
-    }
-
-
-
-    return (
+  };
+  return (
+    <div>
+      <div className="p-5 h-screen flex flex-col justify-between">
         <div>
-            <div className="p-7 h-screen flex flex-col justify-between">
-                <div>
-                    <img className="w-16 mb-10" src="" alt="" />
-                    <form onSubmit={(e)=>{
-                        submitHandler(e)
-                    }}>
-                        <h3 className=" text-base font-medium mb-2">What's your name..</h3>
-                        <div className="flex gap-4 mb-6">
-                            <input required
-                        className="bg-[#eeeeee] w-1/2  rounded px-4 py-2 border text-base"
-                        type="text" 
-                        placeholder="First name.."
-                        value={firstName}
-                        onChange={(e)=>{
-                            setFirstName(e.target.value)
-                        }}
-                        />
-                        <input required
-                        className="bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-base "
-                        type="text" 
-                        placeholder="last name.."
-                        value={lastName}
-                        onChange={(e)=>{
-                            setLastName(e.target.value)
-                        }}
-                        />
-                        </div>
-                        
-                        <h3 className=" text-base font-medium mb-2">What's your email..</h3>
-                        <input required
-                        className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border text-base"
-                        type="email" 
-                        placeholder="email@example.com.."
-                        value={email}
-                        onChange={(e)=>{
-                            setEmail(e.target.value)
-                        }}
-                        />
-
-                        <h3 className="text-base font-medium mb-2">What's your Password..</h3>
-                        <input required
-                        className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border text-base"
-                        type="password" 
-                        placeholder="password.."
-                        value={password}
-                        onChange={(e)=>{
-                            setPassword(e.target.value)
-                        }}
-                        />
-                    </form>
-                    <p className="text-center">Already have a account? <Link to='/signup' className='text-blue-600'>Login here</Link></p>
-                </div>
-                <div>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Sunt numquam, voluptatem atque nesciunt vero sed, culpa ipsum dolorum 
-                        rerum dolorem, incidunt ipsam expedita. Vitae, aliquid aspernatur.
-                         Dicta recusandae quasi corrupti?
-                    </p>
-                </div>
+          <img className="w-30" src="./pngegg.png" alt="" />
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
+            <h3 className=" text-base font-medium mb-2">What's your name</h3>
+            <div className="flex gap-4 mb-6">
+              <input
+                required
+                className="bg-[#eeeeee] w-1/2  rounded px-4 py-2 border text-base"
+                type="text"
+                placeholder="First name.."
+                value={firstName}
+                onChange={(e) => {
+                  setFirstName(e.target.value);
+                }}
+              />
+              <input
+                required
+                className="bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-base "
+                type="text"
+                placeholder="last name.."
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+              />
             </div>
+
+            <h3 className="text-base font-medium mb-2">What's your email</h3>
+            <input
+              className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+              required
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              type="email"
+              placeholder="email@example.com.."
+            />
+
+            <h3 className="text-base font-medium mb-2">Enter your password</h3>
+            <input
+              className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+              required
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              type="password"
+              placeholder="password..."
+            />
+            <button className="bg-black text-white mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base">
+              signup
+            </button>
+          </form>
+          <p className="text-center">
+            Already have a account?{" "}
+            <Link to="/login" className="text-blue-600">
+              Login here
+            </Link>
+          </p>
         </div>
-    )
+        <div>
+          <p className="text-gray-500 text-sm">
+            Lorem ipsum Sunt numquam, voluptatem
+            atque nesciunt vero sed, Dicta recusandae quasi corrupti?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
+export default UserSignup;
