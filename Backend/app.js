7119ba288bbd9f3024c3552a3b02219f5720a7b8
Ayush-y01 <Ayush-y01>
2025-12-23 23:16:42 +0530

    ride and map api backend

--- Backend/app.js
+++ Backend/app.js
@@ -9,6 +9,7 @@ const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.route.js');
 const captainRoutes = require('./routes/captain.route.js');
 const mapsRoutes = require('./routes/maps.route.js');
+const rideRoutes = require('./routes/ride.route.js')
 
 connectToDb();
 
@@ -25,5 +26,6 @@ app.get('/',(req,res) => {
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
 
 module.exports = app;
--- Backend/services/ride.service.js
+++ Backend/services/ride.service.js
@@ -1,13 +1,62 @@
 const rideModel = require('../models/ride.model.js');
 const mapsService = require('./maps.service.js');
-
-module.exports.createRide = async ({}) => {}
+const crypto = require('crypto')
 
 
 async function getFare(pickup, destination) {
     if (!pickup || !destination) {
         throw new Error('pickup and destination are required')
     }
+    
+    const distanceTime = await mapsService.getDistanceTime(pickup, destination);
 
-    const distanceTime = await mapsService.getDistanceTime(pickup, destination)
+    const baseFare = {
+        auto: 30,
+        car: 50,
+        bike: 20
+    };
+
+    const perKmRate = {
+        auto: 10,
+        car: 15,
+        bike: 8
+    }
+    const perMinuteRate = {
+        auto: 2,
+        car: 4,
+        bike:1.5
+
+    }
+    const fare = {
+        auto: baseFare.auto + ((distanceTime.distance.value / 1000) * perKmRate.auto) + ((distanceTime.duration.value / 60) * perMinuteRate.auto),
+        car: baseFare.car + ((distanceTime.distance.value / 1000) * perKmRate.car) + ((distanceTime.duration.value / 60) * perMinuteRate.car),
+        bike: baseFare.bike + ((distanceTime.distance.value / 1000) * perKmRate.bike) + ((distanceTime.duration.value / 60) * perMinuteRate.bike) 
+    };
+    return fare;
+}
+
+function getOtp(num) {
+    function generateOtp(num) {
+        const otp = crypto.randomInt(Math.pow(10, num - 1)) // this part was where we stop at night 9:30 PM 23/12/2025 Tuesday
+    }
+}
+
+
+
+module.exports.createRide = async ({user, pickup, destination, vehicleType}) => {
+    
+    if (!user || !pickup || !destination || !vehicleType) {
+        throw new Error('All Field are require');
+    }
+
+    const fare = await getFare(pickup, destination);
+
+    const ride = rideModel.create({
+        user,
+        pickup,
+        destination,
+        fare: fare[ vehicleType ]
+    })
+    return ride;
+
 }
